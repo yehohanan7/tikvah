db3869441aa592215a2a981aef9c6f4b43b57ac6
John <yehohanan7@gmail.com>
2012-09-11 02:12:32 +0530

    node js importer updated

--- db/importer/mongo/node/queries.js
+++ db/importer/mongo/node/queries.js
@@ -1,1 +1,12 @@
-db.products.find({})
+db.products.find({author.name : "test"})
+db.products.find ({categories.product.rating : {$gt : 2}})
+
+db.products.find ({tags : "spftware"})
+db.products.find().sort({date : -1}).limit(10)
+
+
+db.products.ensureIndex({author.name : 1})
+
+db.products.find ({loc: {$near : [50, 50]}})
+
+list of predicates
