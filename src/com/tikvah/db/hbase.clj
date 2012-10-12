f9db61b340a907fc7dd15e3ada3fd48c408d2732
John <yehohanan7@gmail.com>
2012-10-12 06:03:25 +0530

    refactorings

--- src/com/tikvah/db/hbase.clj
+++ src/com/tikvah/db/hbase.clj
@@ -1,6 +1,6 @@
 (ns com.tikvah.db.hbase
-  (:use [com.tikvah.commons.sugar]
-        [com.tikvah.commons.log]))
+  (:use [com.tikvah.commons.sugar]))
+
 
 (import '(org.apache.hadoop.hbase.client HTable Result ResultScanner Scan Put))
 (import '(org.apache.hadoop.hbase.util Bytes))
@@ -43,18 +43,20 @@
   )
 
 
+(defn- toput [row]
+  (let [id (rowid row) [cf label value] (columndata row) put (Put. (tobyte id))]
+     (addcolumn put cf label value)
+    )
+  )
 
 (defn table
   "updates the hbase table with all the rows, prefer to use batch update if you want to do multuple updates"
   [tablename operation & rows]
   (try
-    (let [config (HBaseConfiguration/create) table (findtable config tablename)]
-      (for [r rows :let [id (rowid r) [cf label value] (columndata r) row (Put. (tobyte id))]]
-        (-> row (addcolumn cf label value) (insert-to table)) 
-        )
+    (let [config (HBaseConfiguration/create) table (findtable config tablename) puts (map #(toput %) rows) ]
+      (.put table puts)
       )
-    (catch Exception e (error e (str tablename operation rows))))
-  
+    (catch Exception e (println e (str tablename operation rows))))
  )
 
 
@@ -62,14 +64,14 @@
   "does a batch update to hbase"
   [& xs]
   (binding [*autoflush* false]
-    (map #(apply htable %) xs)
+    (map #(apply table %) xs)
     )
   )
 
 
 
 (defn test-me []
-  (htable "test" :put {"rr" ["cf" "name" "value"]})
+  ;;(htable "test" :put {"rr" ["cf" "name" "value"]})
   )
 
 
