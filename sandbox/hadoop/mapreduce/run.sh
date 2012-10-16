e22f8b3ad66d4617ce826ae3cf4a6c7bdb74fdb7
John <yehohanan7@gmail.com>
2012-10-17 04:54:10 +0530

    config changes to make hadoop work in standalone mode

--- sandbox/hadoop/mapreduce/run.sh
+++ sandbox/hadoop/mapreduce/run.sh
@@ -1,1 +1,3 @@
-hadoop com.tikvah.jobs.samples.WordCounter -conf conf/hadoop-localhost.xml
+#start-all.sh
+rm -r wordcounts
+hadoop jar target/batch-1.0-SNAPSHOT.jar com.tikvah.jobs.samples.WordCounter
--- sandbox/hadoop/mapreduce/setenv.sh
+++ sandbox/hadoop/mapreduce/setenv.sh
@@ -5,3 +5,5 @@ export PATH=$PATH:$HADOOP_INSTALL/bin
 
 export HADOOP_OPTS="-Djava.security.krb5.realm=OX.AC.UK -Djava.security.krb5.kdc=kdc0.ox.ac.uk:kdc1.ox.ac.uk"
 
+
+
