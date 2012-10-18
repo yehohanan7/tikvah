a56b98c3a04fa6fb18b3a71bc14323cdbb252b64
John <yehohanan7@gmail.com>
2012-10-18 17:46:45 +0100

    facts logging added

--- src/com/tikvah/facts/core.clj
+++ src/com/tikvah/facts/core.clj
@@ -1,7 +1,12 @@
 (ns com.tikvah.facts.core)
 
+
+(defn- string-format [& xs]
+  (apply str (interpose " " (map #(if (keyword? %) (name %) %) xs)))
+  )
+
 (defn fact [fact-type fact-identity [subject verb object]]
-  (apply str (interpose " " ["recording fact.." fact-identity "'s " subject verb object]))
+  (string-format fact-identity "'s " subject verb object)
   )
 
 
